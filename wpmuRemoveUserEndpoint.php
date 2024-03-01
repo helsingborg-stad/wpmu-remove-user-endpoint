26bb7a7ee9ca5049b9647f5f7392d4611bbb529b
NiclasNorin <niclasn@outlook.com>
2024-03-01 09:18:01 +0000

    chore: adding a better error description

--- wpmuRemoveUserEndpoint.php
+++ wpmuRemoveUserEndpoint.php
@@ -9,7 +9,7 @@ Author:         Niclas Norin
 function removeUserEndpointIfNotLoggedIn() {
     if (!is_user_logged_in()) {
         add_filter('rest_prepare_user', function ($response, $user, $request) {
-            return new WP_Error('rest_forbidden', 'We are no longer friends.', array('status' => 403));
+            return new WP_Error('rest_forbidden', 'Forbidden: Access to the requested resource is forbidden. You may not have the necessary permissions to access this resource. Please contact the administrator if you believe this is an error.', array('status' => 403));
         }, 10, 3);
     }
 }
