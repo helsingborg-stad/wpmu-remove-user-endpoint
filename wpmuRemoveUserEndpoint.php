0fc43965e7017d62da8e9622e4ab5e371c523f26
Thor Brink <thor.brink@helsingborg.se>
2026-04-24 06:27:21 +0000

    fix: returning error throws error further up the call stack

--- wpmuRemoveUserEndpoint.php
+++ wpmuRemoveUserEndpoint.php
@@ -6,12 +6,15 @@ Version:        1.0.0
 Author:         Niclas Norin
 */
 
-function removeUserEndpointIfNotLoggedIn() {
-    if (!is_user_logged_in()) {
-        add_filter('rest_prepare_user', function ($response, $user, $request) {
-            return new WP_Error('rest_forbidden', 'Forbidden: Access to the requested resource is forbidden. You may not have the necessary permissions to access this resource. Please contact the administrator if you believe this is an error.', array('status' => 403));
-        }, 10, 3);
+add_action('init', function () {
+    if (is_user_logged_in()) {
+        return;
     }
-}
 
-add_action('init', 'removeUserEndpointIfNotLoggedIn');
+    add_filter('rest_endpoints', function (array $endpoints): array {
+        unset($endpoints['/wp/v2/users']);
+        unset($endpoints['/wp/v2/users/(?P<id>[\d]+)']);
+        unset($endpoints['/wp/v2/users/me']);
+        return $endpoints;
+    });
+});
